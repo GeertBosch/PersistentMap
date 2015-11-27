bad910f27e53781965d654612075e1045406a945
Geert Bosch <bosch@gnat.com>
2015-11-27 09:35:51 -0500

    Initial Commit

---
+++ PersistentMap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PersistentMap
+//
+//  Created by Geert Bosch on 11/27/15.
+//  Copyright © 2015 MongoDB. All rights reserved.
+//
+
+#include <iostream>
+#include "persistent_map.h"
+
+int main(int argc, const char * argv[]) {
+    persistent::map<int, int> M;
+    return 0;
+}
